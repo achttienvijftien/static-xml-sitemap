e78bb0cbc00c4ca596330fc4697540edeeafe710
Tobias van Dijk <tobias@1815.nl>
2025-10-30 23:09:19 +0100

    feat: add support for yoast news sitemap xsl

--- src/Compatibility/YoastNewsSeo/YoastNewsSeo.php
+++ src/Compatibility/YoastNewsSeo/YoastNewsSeo.php
@@ -19,6 +19,9 @@ class YoastNewsSeo {
 	public function add_hooks(): void {
 		add_filter( 'static_sitemap_index_content', [ $this, 'index_content' ] );
 		add_action( 'static_sitemap_request', [ $this, 'request' ], 10, 3 );
+
+		add_filter( 'query_vars', [ $this, 'add_query_vars' ] );
+		add_action( 'pre_get_posts', [ $this, 'xsl_request' ], 1 );
 	}
 
 	/**
@@ -43,10 +46,11 @@ class YoastNewsSeo {
 	public function request( $query, string $type, int $paged ): void {
 		global $wpseo_sitemaps;
 
-		if ( ! $query instanceof \WP_Query
-			|| ! $wpseo_sitemaps instanceof \WPSEO_Sitemaps
-			|| $type !== $this->get_sitemap_type()
-		) {
+		if ( ! $wpseo_sitemaps instanceof \WPSEO_Sitemaps || ! method_exists( $wpseo_sitemaps, 'redirect' ) ) {
+			return;
+		}
+
+		if ( $type !== $this->get_sitemap_type() ) {
 			return;
 		}
 
@@ -65,6 +69,51 @@ class YoastNewsSeo {
 		}
 
 		return \WPSEO_News_Sitemap::get_sitemap_name( false );
+	}
+
+	/**
+	 * @param array|mixed $query_vars
+	 *
+	 * @return array|mixed
+	 */
+	public function add_query_vars( $query_vars ) {
+		if ( ! is_array( $query_vars ) ) {
+			return $query_vars;
+		}
+
+		if ( ! in_array( 'yoast-sitemap-xsl', $query_vars, true ) ) {
+			$query_vars[] = 'yoast-sitemap-xsl';
+		}
+
+		return $query_vars;
+	}
+
+	/**
+	 * Handle requests for News sitemap XSL files.
+	 *
+	 * @param \WP_Query|mixed $query Main query instance.
+	 *
+	 * @return void
+	 */
+	public function xsl_request( $query ) {
+		global $wpseo_sitemaps;
+
+		if ( ! $wpseo_sitemaps instanceof \WPSEO_Sitemaps || ! method_exists( $wpseo_sitemaps, 'xsl_output' ) ) {
+			return;
+		}
+
+		if ( ! $query instanceof \WP_Query || ! $query->is_main_query() ) {
+			return;
+		}
+
+		$yoast_sitemap_xsl = get_query_var( 'yoast-sitemap-xsl' );
+
+		if ( $yoast_sitemap_xsl !== $this->get_sitemap_type() ) {
+			return;
+		}
+
+		$wpseo_sitemaps->xsl_output( $yoast_sitemap_xsl );
+		exit();
 	}
 
 }
